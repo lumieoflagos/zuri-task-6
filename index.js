b62f03eb47e1efad3a86c3a7a3e7173ddef4f210
Olumide Ojediran <olumide9967@gmail.com>
2022-06-25 16:26:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let myName = 'Name: Ojediran Olumide\n';
+console.log(myName);
+
+let height = 'Height: 186cm\n';
+console.log(height);
+
+let country = 'Country: Nigeria ';
+console.log(country);
+
+alert (myName + height + country);
